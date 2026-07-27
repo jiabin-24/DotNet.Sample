38396f6580452bdefdd5e594773959f4641f1ce6
Jiabin <v-jiabinzhou@microsoft.com>
2026-07-27 09:51:05 +0800

    Remove Fusion logging scripts

--- scripts/stop-funsion.ps1
+++
@@ -1,5 +0,0 @@
-
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Fusion" -Name "EnableLog" -Value 0 -Type DWord
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Fusion" -Name "ForceLog" -Value 0 -Type DWord
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Fusion" -Name "LogFailures" -Value 0 -Type DWord
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Fusion" -Name "LogResourceBinds" -Value 0 -Type DWord
